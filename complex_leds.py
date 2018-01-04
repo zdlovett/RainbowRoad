31e0e90edc292fce9422100312efe8a894bc9dfd
Zach Lovett <zdlovett@gmail.com>
2018-01-04 01:48:24 -0800

    moved notes into file for more complex animations

---
+++ complex_leds.py
@@ -0,0 +1,13 @@
+
+"""
+Ideas:
+Warp core:
+pulse that moves in from the top and bottom and then bright 
+at the gpu when it meets in the middle
+
+Rain:
+blue rain down the side, with white flashes at the top,
+blue ripples on the bottom, and soft lighting from the gpu
+
+Voice / sound based something or other
+"""
--- simple_leds.py
+++ simple_leds.py
@@ -7,20 +7,6 @@ from cpu_leds import LEDS, find_device
 
 NUM_LEDS = 390
 
-
-"""
-Ideas:
-Warp core:
-pulse that moves in from the top and bottom and then bright 
-at the gpu when it meets in the middle
-
-Rain:
-blue rain down the side, with white flashes at the top,
-blue ripples on the bottom, and soft lighting from the gpu
-
-Voice / sound based something or other
-"""
-
 def breath():
     colors = np.zeros( (NUM_LEDS, 3))
     while True:
